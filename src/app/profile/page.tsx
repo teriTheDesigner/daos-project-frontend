fab4692b02c19bc2aa35a3c4a2918ef7a608d585
teriTheDesigner <tere1109@stud.kea.dk>
2024-12-12 20:50:10 +0100

    Update profile page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -86,6 +86,10 @@ export default function Profile() {
     }
   };
 
+  function createEnsamble() {
+    router.push("/create-ensemble");
+  }
+
   const handleLogin = () => {
     router.push("/login");
   };
@@ -94,24 +98,65 @@ export default function Profile() {
       <Nav />
       <div className="outer-grid montserrat-regular">
         <div className="inner-grid">
-          <h1
-            className="oswald-medium col-start-1 col-end-13 mb-4 mt-12 text-4xl"
-            style={{ color: "var(--dark-blue)" }}
-          >
-            Profile
-          </h1>
           {!isLoggedIn ? (
             <div className="col-start-1 col-end-5 my-8 flex flex-col gap-8">
+              <h1
+                className="oswald-medium mb-4 mt-12 text-4xl"
+                style={{ color: "var(--dark-blue)" }}
+              >
+                Profile
+              </h1>
               <p> Please log in to view your profile.</p>
               <PrimaryButton color="blue" size="large" onClick={handleLogin}>
                 Log in
               </PrimaryButton>
             </div>
           ) : (
-            <div className="col-start-1 col-end-13">
-              <p>Welcome back {userData?.name}</p>
-              <h2>Your Ensembles:</h2>
-              <div className="grid grid-cols-3 gap-6 py-12">
+            <div className="col-start-1 col-end-13 mb-10 mt-12">
+              <div className="flex gap-12 border-b border-gray-200 pb-10">
+                <div className="rounded-xl bg-white p-3 shadow-md shadow-gray-200">
+                  <div className="rounded-lg bg-gray-100 p-12">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="30.489"
+                      height="31.876"
+                      viewBox="0 0 30.489 31.876"
+                    >
+                      <path
+                        id="icons8-user"
+                        d="M19.245,3a8.315,8.315,0,0,0-8.315,8.315V12.7a8.315,8.315,0,0,0,16.631,0V11.315A8.315,8.315,0,0,0,19.245,3Zm0,22.175c-5.552,0-12.676,3-14.725,5.668a2.517,2.517,0,0,0,2.017,4.033h25.42a2.517,2.517,0,0,0,2.017-4.033C31.921,28.179,24.794,25.175,19.242,25.175Z"
+                        transform="translate(-4 -3)"
+                        fill="#353a5d"
+                      />
+                    </svg>
+                  </div>
+                </div>
+                <div className="flex flex-col gap-4">
+                  <h3
+                    className="oswald-medium mb-4 text-4xl"
+                    style={{ color: "var(--dark-red)" }}
+                  >
+                    {userData?.name}
+                  </h3>
+                  <p className="text-gray-500">{userData?.email}</p>
+                </div>
+              </div>
+              <div className="mb-4 mt-8 flex justify-between">
+                <h2
+                  className="oswald-medium text-4xl"
+                  style={{ color: "var(--dark-blue)" }}
+                >
+                  Mine opslag
+                </h2>
+                <PrimaryButton
+                  onClick={createEnsamble}
+                  color="white"
+                  size="medium"
+                >
+                  Create Ensemble
+                </PrimaryButton>
+              </div>
+              <div className="grid grid-cols-3 gap-6 py-8">
                 {ensembles.length > 0 ? (
                   ensembles.map((ensemble) => (
                     <div key={ensemble._id}>
@@ -126,7 +171,30 @@ export default function Profile() {
                     </div>
                   ))
                 ) : (
-                  <p>No ensembles available.</p>
+                  <div className="col-start-1 col-end-13 mx-auto flex flex-col place-items-center gap-6">
+                    <img
+                      className="w-40"
+                      src="/red-notebook.png"
+                      alt="red notebook"
+                    />
+                    <h4
+                      className="montserrat-bold"
+                      style={{ color: "var(--dark-blue)" }}
+                    >
+                      Du har ingen opslag endnu
+                    </h4>
+                    <p className="max-w-[400px] text-center text-gray-400">
+                      Opret et opslag så du kan finde, eller blive fundet af
+                      andre musikere.
+                    </p>
+                    <PrimaryButton
+                      onClick={createEnsamble}
+                      color="blue"
+                      size="large"
+                    >
+                      Create Ensemble
+                    </PrimaryButton>
+                  </div>
                 )}
               </div>
             </div>
